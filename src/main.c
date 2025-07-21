e48563e59535ff66a80842444988c5581799b2ad
David Balzer <dmbalzer@gmail.com>
2025-07-20 20:28:38 -0400

    added map.h map.c

--- src/main.c
+++ src/main.c
@@ -2,31 +2,17 @@
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
+#include "map.h"
+
 #define SCREEN_W 320
 #define SCREEN_H 240
 
 #define WINDOW_W (SCREEN_W*4)
 #define WINDOW_H (SCREEN_H*4)
 
-#define MAP_W 15
-#define MAP_H 11
-#define TILE_SIZE 32
-static const int map[MAP_W * MAP_H] = {
-	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-	1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
-	1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-};
 
 static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+SDL_Renderer* renderer = NULL;
 static float frametime = 0.0f;
 static bool quit = false;
 
@@ -35,32 +21,6 @@ void sdl_do_events(void);
 void sdl_begin_draw(void);
 void sdl_end_draw(void);
 void sdl_quit(void);
-
-void map_draw(void)
-{
-	SDL_SetRenderDrawColor(renderer, 0xFF,0xFF,0xFF,0xFF);
-	for ( int x = 0; x < MAP_W; x++ )
-	{
-		for ( int y = 0; y < MAP_H; y++ )
-		{
-			if ( map[ x + y * MAP_W ] == 0 )
-			{
-				continue;
-			}
-			SDL_FRect dst = (SDL_FRect){ x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE };
-			SDL_RenderFillRect(renderer, &dst);
-		}
-	}
-	SDL_SetRenderDrawColor(renderer, 0x0F,0x0F,0x0F,0xFF);
-	for ( int x = 0; x < MAP_W; x++ )
-	{
-		for ( int y = 0; y < MAP_H; y++ )
-		{
-			SDL_RenderLine(renderer, x * TILE_SIZE, 0, x * TILE_SIZE, MAP_H * TILE_SIZE);
-			SDL_RenderLine(renderer, 0, y * TILE_SIZE, MAP_W * TILE_SIZE, y * TILE_SIZE);
-		}
-	}
-}
 
 int main(void)
 {
---
+++ src/map.h
@@ -0,0 +1,10 @@
+#ifndef MAP_INCLUDED
+#define MAP_INCLUDED
+
+#define MAP_W 15
+#define MAP_H 11
+#define TILE_SIZE 32
+
+void map_draw(void);
+
+#endif // MAP_INCLUDED
