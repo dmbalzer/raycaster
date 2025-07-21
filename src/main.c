40b261c93ecb46855ea8fc68ff86926863bcfb11
David Balzer <dmbalzer@gmail.com>
2025-07-20 20:33:19 -0400

    refactored sdl

--- src/main.c
+++ src/main.c
@@ -1,26 +1,11 @@
-#include <SDL3/SDL.h>
+#include <stdbool.h>
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
+#include "sdl.h"
 #include "map.h"
 
-#define SCREEN_W 320
-#define SCREEN_H 240
-
-#define WINDOW_W (SCREEN_W*4)
-#define WINDOW_H (SCREEN_H*4)
-
-
-static SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-static float frametime = 0.0f;
-static bool quit = false;
-
-void sdl_init(void);
-void sdl_do_events(void);
-void sdl_begin_draw(void);
-void sdl_end_draw(void);
-void sdl_quit(void);
+bool quit = false;
 
 int main(void)
 {
@@ -40,53 +25,3 @@ int main(void)
 	sdl_quit();
 	return 0;
 }
-
-void sdl_init(void)
-{
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_CreateWindowAndRenderer("Raycaster", WINDOW_W, WINDOW_H, 0, &window, &renderer);
-	SDL_SetRenderVSync(renderer, 1);
-}
-
-void sdl_do_events(void)
-{
-	SDL_Event event;
-	while ( SDL_PollEvent(&event) )
-	{
-		switch ( event.type )
-		{
-			case SDL_EVENT_QUIT:
-				quit = true;
-			break;
-			case SDL_EVENT_KEY_DOWN:
-				if ( event.key.key == SDLK_ESCAPE )
-				{
-					quit = true;
-				}
-			break;
-		}
-	}
-}
-
-void sdl_begin_draw(void)
-{
-	SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0xFF);
-	SDL_RenderClear(renderer);
-}
-
-void sdl_end_draw(void)
-{
-	SDL_RenderPresent(renderer);
-	static Uint64 prev = 0;
-	const Uint64 now = SDL_GetTicks();
-	frametime = ( prev - now ) /1000.0f;
-	prev = now;
-}
-
-void sdl_quit(void)
-{
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
-
---
+++ src/sdl.c
@@ -0,0 +1,57 @@
+#include "sdl.h"
+#include <SDL3/SDL.h>
+
+static SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+static float frametime = 0.0f;
+extern bool quit;
+
+void sdl_init(void)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer("Raycaster", WINDOW_W, WINDOW_H, 0, &window, &renderer);
+	SDL_SetRenderVSync(renderer, 1);
+}
+
+void sdl_do_events(void)
+{
+	SDL_Event event;
+	while ( SDL_PollEvent(&event) )
+	{
+		switch ( event.type )
+		{
+			case SDL_EVENT_QUIT:
+				quit = true;
+			break;
+			case SDL_EVENT_KEY_DOWN:
+				if ( event.key.key == SDLK_ESCAPE )
+				{
+					quit = true;
+				}
+			break;
+		}
+	}
+}
+
+void sdl_begin_draw(void)
+{
+	SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0xFF);
+	SDL_RenderClear(renderer);
+}
+
+void sdl_end_draw(void)
+{
+	SDL_RenderPresent(renderer);
+	static Uint64 prev = 0;
+	const Uint64 now = SDL_GetTicks();
+	frametime = ( prev - now ) /1000.0f;
+	prev = now;
+}
+
+void sdl_quit(void)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
