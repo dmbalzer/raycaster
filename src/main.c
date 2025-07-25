0f2bf5f42341319f264b30174951f645e7e3ae8a
David Balzer <dmbalzer@gmail.com>
2025-07-25 19:09:11 -0400

    decided to start over again

--- src/main.c
+++ src/main.c
@@ -2,26 +2,7 @@
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
-#include "sdl.h"
-#include "map.h"
-
-bool quit = false;
-
 int main(void)
 {
-	sdl_init();
-	
-	while ( !quit )
-	{
-		sdl_do_events();
-
-		sdl_begin_draw();
-
-		map_draw();
-
-		sdl_end_draw();
-	}
-	
-	sdl_quit();
 	return 0;
 }
