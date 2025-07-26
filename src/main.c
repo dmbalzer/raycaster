ede61003f88f449385b3c2c3789073aa598330fc
David Balzer <dmbalzer@gmail.com>
2025-07-26 09:49:48 -0400

    sdl backend

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
 #include <stdbool.h>
+#include "sdl.h"
 #define RAYMATH_IMPLEMENTATION
 #include "raymath.h"
 
+bool quit = false;
+
 int main(void)
 {
+	sdl_init();
+	while ( !quit )
+	{
+		sdl_do_events();
+		sdl_begin_draw();
+		sdl_end_draw();
+	}
+
+	sdl_quit();
 	return 0;
 }
--- src/sdl.c
+++ src/sdl.c
@@ -1,23 +1,62 @@
+#include "defs.h"
 #include "sdl.h"
 #include <SDL3/SDL.h>
 
+static SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+extern bool quit;
+float frametime = 0.0f;
 
 void sdl_init(void)
 {
+	if ( !SDL_Init(SDL_INIT_VIDEO) )
+	{
+		SDL_Log("%s", SDL_GetError()); exit(-1);
+	}
+	
+	if ( !SDL_CreateWindowAndRenderer("Raycaster", 960,640,0,&window,&renderer) )
+	{
+		SDL_Log("%s", SDL_GetError()); exit(-1);
+	}
+
+	if ( !SDL_SetRenderVSync(renderer, 1) )
+	{
+		SDL_Log("%s", SDL_GetError()); exit(-1);
+	}
 }
 
 void sdl_do_events(void)
 {
+	SDL_Event ev;
+	while ( SDL_PollEvent(&ev) )
+	{
+		switch ( ev.type )
+		{
+			case SDL_EVENT_QUIT:
+				quit = true;
+			break;
+		}
+	}
 }
 
 void sdl_begin_draw(void)
 {
+	SDL_SetRenderDrawColor(renderer, BLACK);
+	SDL_RenderClear(renderer);
 }
 
 void sdl_end_draw(void)
 {
+	SDL_RenderPresent(renderer);
+	static Uint64 prev = 0;
+	const Uint64 now = SDL_GetTicks();
+	frametime = ( prev - now ) / 1000.0f;
+	prev = now;
 }
 
 void sdl_quit(void)
 {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
