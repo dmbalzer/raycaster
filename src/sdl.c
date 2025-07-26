24cf73c6e5b23be852eb17f476e4ef4be8ddbb52
David Balzer <dmbalzer@gmail.com>
2025-07-25 20:56:45 -0400

    added defs

--- src/sdl.c
+++ src/sdl.c
@@ -2,3 +2,22 @@
 #include <SDL3/SDL.h>
 
 
+void sdl_init(void)
+{
+}
+
+void sdl_do_events(void)
+{
+}
+
+void sdl_begin_draw(void)
+{
+}
+
+void sdl_end_draw(void)
+{
+}
+
+void sdl_quit(void)
+{
+}
--- src/sdl.h
+++ src/sdl.h
@@ -1,5 +1,10 @@
 #ifndef SDL_INCLUDED
 #define SDL_INCLUDED
 
+void sdl_init(void);
+void sdl_do_events(void);
+void sdl_begin_draw(void);
+void sdl_end_draw(void);
+void sdl_quit(void);
 
 #endif // SDL_INCLUDED
