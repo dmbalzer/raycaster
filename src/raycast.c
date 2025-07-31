5bfa711adb690de17b0bed565763f43d8b8f07f0
David Balzer <dmbalzer@gmail.com>
2025-07-31 19:11:57 -0400

    drawing rays

--- src/raycast.c
+++ src/raycast.c
@@ -28,29 +28,21 @@ void raycast_update(void)
     /* Rotate direction with right and left keys */
 	dir = Vector2Rotate(dir, ( keys[SDL_SCANCODE_RIGHT] - keys[SDL_SCANCODE_LEFT] ) * frametime);
 	plane = Vector2Rotate(plane, ( keys[SDL_SCANCODE_RIGHT] - keys[SDL_SCANCODE_LEFT] ) * frametime);
-
-	/* Update direction and plane position vectors */
-	dir_end = Vector2Add(pos, Vector2Scale(dir, 40));
-	plane_start = Vector2Subtract(dir_end, Vector2Scale(plane, 40));
-	plane_end = Vector2Add(dir_end, Vector2Scale(plane, 40));
 }
 
 void raycast_draw(void)
 {
-	/* Draw Scaled Direction Vector */
-	SDL_SetRenderDrawColor(renderer, GREEN);
-	SDL_RenderLine(renderer, pos.x, pos.y, dir_end.x, dir_end.y);
-	
-	/* Draw Scaled Plane Vector */
-	SDL_SetRenderDrawColor(renderer, RED);
-	SDL_RenderLine(renderer, plane_start.x, plane_start.y, plane_end.x, plane_end.y);
-	
 	/* Draw Rays */
-	SDL_SetRenderDrawColor(renderer, BLUE);
-	Vector2 cam = dir_end;
-	for ( int i = 0; i < 10; i++ )
+	SDL_SetRenderDrawColor(renderer, GREEN);
+	for ( int i = 0; i < SCREEN_W; i++ )
 	{
-		SDL_RenderLine(renderer, pos.x, pos.y, cam.x, cam.y);
-		cam = Vector2Add(cam, Vector2Scale(plane, 1/10));
+		float cam_x = 2 * (i / (float)SCREEN_W) - 1;
+		
+		Vector2 cam_dir = {
+			.x = dir.x + plane.x * cam_x,
+			.y = dir.y + plane.y * cam_x
+		};
+		Vector2 cam_end = Vector2Add(pos, Vector2Scale(cam_dir, 1000));
+		SDL_RenderLine(renderer, pos.x, pos.y, cam_end.x, cam_end.y);
 	}
 }
